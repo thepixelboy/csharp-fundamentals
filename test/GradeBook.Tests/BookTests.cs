d5472c9f30c35f6b8ba6deee4f0b6937dff45657
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-06-05 17:22:38 +0200

    Chapter 7. Controlling the Flow of Execution

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -3,16 +3,17 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class BookTests
+    public class Typetests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAnAverageGrade()
         {
             // arrange
             var book = new Book("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
+            book.AddGrade(105);
             
             // act
             var result = book.GetStatistics();
@@ -21,6 +22,7 @@ namespace GradeBook.Tests
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High, 1);
             Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.Letter);
         }
     }
 }
